54b23a6d645f6f0183b1b17a6d47fe5704265ff8
Stael Tchinda <staelwilfried@gmail.com>
2025-01-10 15:56:21 +0100

    Add GameBoardService with board creation and cell liveness logic tests

---
+++ src/app/services/game-board.service.spec.ts
@@ -0,0 +1,158 @@
+import { TestBed } from "@angular/core/testing";
+
+import { GameBoardService } from "./game-board.service";
+
+describe("BoardBoardService", () => {
+  let service: GameBoardService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(GameBoardService);
+  });
+
+  it("should be created", () => {
+    expect(service).toBeTruthy();
+  });
+
+  it("should initialise board board", () => {
+    const boardWidth = 2;
+    const boardHeight = 2;
+    const board = service.createBoard(boardWidth, boardHeight);
+
+    expect(board.width).toBe(boardWidth);
+    expect(board.height).toBe(boardHeight);
+    expect(board.grid.length).toBe(boardWidth);
+    expect(board.grid[0].length).toBe(boardHeight);
+  });
+
+  it("should have dead cells at start", () => {
+    const boardWidth = 2;
+    const boardHeight = 2;
+    const board = service.createBoard(boardWidth, boardHeight);
+
+    expect(service.isCellAlive(board, 0, 0)).toBe(false);
+    expect(service.isCellAlive(board, 0, 1)).toBe(false);
+    expect(service.isCellAlive(board, 1, 0)).toBe(false);
+    expect(service.isCellAlive(board, 1, 1)).toBe(false);
+  });
+
+  it("should toggle cell liveness", () => {
+    const boardWidth = 3;
+    const boardHeight = 3;
+    const cellX = 1;
+    const cellY = 1;
+    let board = service.createBoard(boardWidth, boardHeight);
+
+    expect(service.isCellAlive(board, cellX, cellY)).toBe(false);
+
+    board = service.toggleCellLiveness(board, cellX, cellY);
+
+    expect(service.isCellAlive(board, cellX, cellY)).toBe(true);
+
+    board = service.toggleCellLiveness(board, cellX, cellY);
+
+    expect(service.isCellAlive(board, cellX, cellY)).toBe(false);
+  });
+
+  describe("board logic", () => {
+    it("dead cell should stay dead if they are all dead", () => {
+      const boardWidth = 2;
+      const boardHeight = 2;
+      let board = service.createBoard(boardWidth, boardHeight);
+
+      expect(service.isCellAlive(board, 0, 0)).toBe(false);
+      expect(service.isCellAlive(board, 0, 1)).toBe(false);
+      expect(service.isCellAlive(board, 1, 0)).toBe(false);
+      expect(service.isCellAlive(board, 1, 1)).toBe(false);
+
+      board = service.tick(board);
+
+      expect(service.isCellAlive(board, 0, 0)).toBe(false);
+      expect(service.isCellAlive(board, 0, 1)).toBe(false);
+      expect(service.isCellAlive(board, 1, 0)).toBe(false);
+      expect(service.isCellAlive(board, 1, 1)).toBe(false);
+    });
+
+    it("alive cell should die if it has less than 2 neighbors", () => {
+      const boardWidth = 3;
+      const boardHeight = 3;
+      const cellX = 1;
+      const cellY = 1;
+      let board = service.createBoard(boardWidth, boardHeight);
+
+      service.toggleCellLiveness(board, cellX, cellY);
+
+      expect(service.isCellAlive(board, cellX, cellY)).toBe(true);
+      expect(service.isCellAlive(board, cellX - 1, cellY - 1)).toBe(false);
+      expect(service.isCellAlive(board, cellX - 1, cellY)).toBe(false);
+
+      board = service.tick(board);
+
+      expect(service.isCellAlive(board, cellX, cellY)).toBe(false);
+    });
+
+    it("alive cell should live if it has 2 neighbors", () => {
+      const boardWidth = 3;
+      const boardHeight = 3;
+      const cellX = 1;
+      const cellY = 1;
+      let board = service.createBoard(boardWidth, boardHeight);
+
+      board = service.toggleCellLiveness(board, cellX, cellY);
+      board = service.toggleCellLiveness(board, cellX - 1, cellY - 1);
+      board = service.toggleCellLiveness(board, cellX - 1, cellY);
+      board = service.tick(board);
+
+      expect(service.isCellAlive(board, cellX, cellY)).toBe(true);
+    });
+
+    it("alive cell should live if it has 3 neighbors", () => {
+      const boardWidth = 3;
+      const boardHeight = 3;
+      const cellX = 1;
+      const cellY = 1;
+      let board = service.createBoard(boardWidth, boardHeight);
+
+      board = service.toggleCellLiveness(board, cellX, cellY);
+      board = service.toggleCellLiveness(board, cellX - 1, cellY - 1);
+      board = service.toggleCellLiveness(board, cellX - 1, cellY);
+      board = service.toggleCellLiveness(board, cellX + 1, cellY);
+      board = service.tick(board);
+
+      expect(service.isCellAlive(board, cellX, cellY)).toBe(true);
+    });
+
+    it("alive cell should die if it has more than 3 neighbors", () => {
+      const boardWidth = 3;
+      const boardHeight = 3;
+      const cellX = 1;
+      const cellY = 1;
+      let board = service.createBoard(boardWidth, boardHeight);
+
+      board = service.toggleCellLiveness(board, cellX, cellY);
+      board = service.toggleCellLiveness(board, cellX - 1, cellY - 1);
+      board = service.toggleCellLiveness(board, cellX - 1, cellY);
+      board = service.toggleCellLiveness(board, cellX + 1, cellY);
+      board = service.toggleCellLiveness(board, cellX, cellY - 1);
+      board = service.tick(board);
+
+      expect(service.isCellAlive(board, cellX, cellY)).toBe(false);
+    });
+
+    it("dead cell should live if it has exactly 3 neighbors", () => {
+      const boardWidth = 3;
+      const boardHeight = 3;
+      const cellX = 1;
+      const cellY = 1;
+      let board = service.createBoard(boardWidth, boardHeight);
+
+      board = service.toggleCellLiveness(board, cellX - 1, cellY - 1);
+      board = service.toggleCellLiveness(board, cellX - 1, cellY);
+      board = service.toggleCellLiveness(board, cellX + 1, cellY);
+      board = service.tick(board);
+
+      expect(service.isCellAlive(board, cellX, cellY)).toBe(true);
+    });
+
+  });
+});
---
+++ src/app/services/game-board.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from "@angular/core";
+
+export interface GameBoard {
+  width: number;
+  height: number;
+
+  grid: boolean[][];
+}
+
+@Injectable({
+  providedIn: "root",
+})
+export class GameBoardService {
+  constructor() {}
+
+  createBoard(width: number, height: number): GameBoard {
+    return {
+      width,
+      height,
+      grid: Array.from({ length: width }, () => Array.from({ length: height }, () => false)),
+    } as GameBoard;
+  }
+
+  isCellAlive(board: GameBoard, x: number, y: number): boolean {
+    return board.grid[x][y];
+  }
+
+  toggleCellLiveness(board: GameBoard, x: number, y: number): GameBoard {
+    board.grid[x][y] = !board.grid[x][y];
+    return board;
+  }
+
+  private countNeighbors(board: GameBoard, x: number, y: number, livenessState: boolean): number {
+    let count = 0;
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        if (i === 0 && j === 0) {
+          continue;
+        }
+
+        const xIndex = x + i;
+        const yIndex = y + j;
+
+        if (xIndex >= 0 && xIndex < board.width && yIndex >= 0 && yIndex < board.height) {
+          if (this.isCellAlive(board, xIndex, yIndex) === livenessState) {
+            count++;
+          }
+        }
+      }
+    }
+    return count;
+  }
+
+  private countAliveNeighbors(board: GameBoard, x: number, y: number): number {
+    return this.countNeighbors(board, x, y, true);
+  }
+
+  tick(board: GameBoard): GameBoard {
+    const newGrid = Array.from({ length: board.width }, () => Array.from({ length: board.height }, () => false));
+
+    for (let i = 0; i < board.grid.length; i++) {
+      for (let j = 0; j < board.grid[i].length; j++) {
+        const aliveNeighbors = this.countAliveNeighbors(board, i, j);
+
+        if (this.isCellAlive(board, i, j) && aliveNeighbors < 2) {
+          newGrid[i][j] = false;
+        } else if (this.isCellAlive(board, i, j) && aliveNeighbors in [2, 3]) {
+          newGrid[i][j] = true;
+        } else if (this.isCellAlive(board, i, j) && aliveNeighbors > 3) {
+          newGrid[i][j] = false;
+        } else if (!this.isCellAlive(board, i, j) && aliveNeighbors === 3) {
+          newGrid[i][j] = true;
+        } else {
+          newGrid[i][j] = board.grid[i][j];
+        }
+      }
+      
+    }
+    return { ...board, grid: newGrid };
+  }
+}
