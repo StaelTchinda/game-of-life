603a831476963a72d7c0b320a429506e46615d2e
Stael Tchinda <staelwilfried@gmail.com>
2025-01-10 14:23:23 +0100

    Update Jest configuration and TypeScript baseUrl for improved module resolution

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   testEnvironment: "node",
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    "^.+.tsx?$": ["ts-jest", {}],
   },
+  moduleDirectories: ["node_modules", "src"],
 };
