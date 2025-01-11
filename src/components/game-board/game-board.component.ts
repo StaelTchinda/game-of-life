f20ba742a954cf45ac53218809f07ec33b13781a
StaelTchinda <45164908+StaelTchinda@users.noreply.github.com>
2025-01-11 12:50:11 +0100

    Add GameBoardControlsComponent and integrate running state management

--- src/components/game-board/game-board.component.ts
+++ src/components/game-board/game-board.component.ts
@@ -1,23 +1,51 @@
-import { Component } from '@angular/core';
-import { GameBoardTableComponent } from 'components/game-board-table/game-board-table.component';
-import { GameBoard } from 'models/game-board';
-import { GameBoardService } from 'services/game-board.service';
+import { Component } from "@angular/core";
+import { GameBoardControlsComponent } from "components/game-board-controls/game-board-controls.component";
+import { GameBoardTableComponent } from "components/game-board-table/game-board-table.component";
+import { GameBoard } from "models/game-board";
+import { GameBoardService } from "services/game-board.service";
 
 @Component({
-  selector: 'app-game-board',
+  selector: "app-game-board",
   standalone: true,
-  imports: [GameBoardTableComponent],
+  imports: [GameBoardTableComponent, GameBoardControlsComponent],
   providers: [GameBoardService],
-  templateUrl: './game-board.component.html',
-  styleUrl: './game-board.component.scss'
+  templateUrl: "./game-board.component.html",
+  styleUrl: "./game-board.component.scss",
 })
 export class GameBoardComponent {
   board: GameBoard;
-  running: boolean;
+  running: boolean = false;
 
   constructor(private gameBoardService: GameBoardService) {
     this.board = gameBoardService.createBoard(10, 10);
     this.running = true;
     console.log("Game board created", this.board);
+  }
+
+  onRunningChange(newRunning: boolean) {
+    console.log("Game running changed", newRunning);
+    if (newRunning) {
+      this.startGame();
+    } else {
+      this.stopGame();
+    }
+    this.running = newRunning;
+  }
+
+  private startGame() {
+    console.log("Game started");
+    this.boardUpdateInterval = setInterval(() => {
+      console.log("Game tick");
+      const newBoard = this.gameBoardService.tick(this.board);
+      console.log("Game board updated", this.board, newBoard);
+      this.board = newBoard;
+    }, this.speed);
+  }
+
+  private stopGame() {
+    console.log("Game paused");
+    if (this.boardUpdateInterval) {
+      clearInterval(this.boardUpdateInterval);
+    }
   }
 }
