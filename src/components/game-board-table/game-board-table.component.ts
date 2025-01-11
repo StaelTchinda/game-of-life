abde5eebc4369f35f4514ddbe259454aee0216b9
StaelTchinda <45164908+StaelTchinda@users.noreply.github.com>
2025-01-11 11:47:37 +0100

    Refactor GameBoardTableComponent to use double quotes and set default value for running input

--- src/components/game-board-table/game-board-table.component.ts
+++ src/components/game-board-table/game-board-table.component.ts
@@ -1,17 +1,17 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { Input } from '@angular/core';
-import { GameBoardTableCellComponent } from 'components/game-board-table-cell/game-board-table-cell.component';
-import { GameBoard } from 'models/game-board';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { Input } from "@angular/core";
+import { GameBoardTableCellComponent } from "components/game-board-table-cell/game-board-table-cell.component";
+import { GameBoard } from "models/game-board";
 
 @Component({
-  selector: 'app-game-board-table',
+  selector: "app-game-board-table",
   standalone: true,
   imports: [GameBoardTableCellComponent, CommonModule],
-  templateUrl: './game-board-table.component.html',
-  styleUrl: './game-board-table.component.scss'
+  templateUrl: "./game-board-table.component.html",
+  styleUrl: "./game-board-table.component.scss",
 })
 export class GameBoardTableComponent {
   @Input() board!: GameBoard;
-  @Input() running!: boolean;
+  @Input() running: boolean = false;
 }
