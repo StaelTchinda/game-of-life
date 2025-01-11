c86400cc2a9fb163082f3a7e6ce49790c6de6042
StaelTchinda <45164908+StaelTchinda@users.noreply.github.com>
2025-01-11 12:55:38 +0100

    Add unit tests for GameBoardComponent logic and board randomisation

--- src/components/game-board/game-board.component.spec.ts
+++ src/components/game-board/game-board.component.spec.ts
@@ -1,23 +1,56 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { GameBoardComponent } from './game-board.component';
+import { GameBoardComponent } from "./game-board.component";
 
-describe('GameBoardComponent', () => {
+describe("GameBoardComponent", () => {
   let component: GameBoardComponent;
   let fixture: ComponentFixture<GameBoardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GameBoardComponent]
-    })
-    .compileComponents();
-    
+      imports: [GameBoardComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(GameBoardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  describe("Logic", () => {
+    it("should create a game board on initialisation", () => {
+      expect(component.board).toBeTruthy();
+    });
+
+    it("should not start the game on initialisation", () => {
+      expect(component.running).toBeFalse();
+    });
+
+    it("should start the game when running changes to true", () => {
+      component.onRunningChange(true);
+      expect(component.running).toBeTrue();
+    });
+
+    it("should stop the game when running changes to false", () => {
+      component.onRunningChange(false);
+      expect(component.running).toBeFalse();
+    });
+
+    it("should update the game board when the game is running", () => {
+      component.onRunningChange(true);
+      const initialBoard = component.board;
+      setTimeout(() => {
+        expect(component.board).not.toEqual(initialBoard);
+      }, component.speed * 1.5);
+    });
+
+    it("should randomise the game board", () => {
+      const initialBoard = component.board;
+      component.onBoardRandomise();
+      expect(component.board).not.toEqual(initialBoard);
+    });
   });
 });
